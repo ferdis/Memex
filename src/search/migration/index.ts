e9d8acf369c1d39170e0eedaf328fd53d4643df8
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-04-11 12:37:23 +0700

    Write master migration entrypoint function
    - super simple, just a for loop for now
    - later would like to give it some state to record progress

--- src/search/migration/index.ts
+++ src/search/migration/index.ts
@@ -1,17 +1,25 @@
+import whenAllSettled from 'when-all-settled'
+
 import exportOldPages from '../search-index-old/export'
 import importNewPage from '../search-index-new/import'
 
-export default function migrate() {
-    const exportChunk = exportOldPages({ chunkSize: 10 }).next
+window['exp'] = exportOldPages
+window['imp'] = importNewPage
+window['migrate'] = migrate
 
-    // return new Promise((resolve, reject) => {
-    //   exportOldPages()
-    //     .on('data', (page: ExportedPage) => {
-    //       importNewPage(page)
-    //     })
-    //     .on('error', reject)
-    //     .on('end', resolve)
-    // })
+export interface MigrateOpts {
+    concurrency: number
+}
+
+const DEF_OPTS: MigrateOpts = {
+    concurrency: 10,
+}
+
+async function migrate({ concurrency = DEF_OPTS.concurrency } = DEF_OPTS) {
+    for await (const pages of exportOldPages({ chunkSize: concurrency })) {
+        await whenAllSettled(pages.map(importNewPage))
+    }
 }
 
 export * from './types'
+export default migrate
