9d06f9aefa61c6d26e63a669116b2f06988b8556
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-04-11 12:00:38 +0700

    Allow partial page exporting if errors in any lookup
    - issues in fetching and reshaping visits and pouch display data will not fail the entire page
    - not fixed; may change this later

--- src/search/search-index-old/export.ts
+++ src/search/search-index-old/export.ts
@@ -29,22 +29,26 @@ async function* exportPages(
 
     do {
         // Get batch for current key + limit, then update the key for next iteration
-        batch = await fetchIndexPageBatch(lastKey, chunkSize)
+        batch = await fetchIndexPageBatch(lastKey, endKey, chunkSize)
         lastKey = [...batch.keys()][batch.size - 1]
 
         // Process each key, fetching needed data, then yielding it to caller
-        yield await whenAllSettled([...batch].map(processKey))
+        const pages: Partial<ExportedPage>[] = await whenAllSettled(
+            [...batch].map(processKey),
+        )
+        yield pages.filter(page => page != null)
     } while (batch.size === chunkSize)
 }
 
 const fetchIndexPageBatch = (
     from: string,
+    to: string,
     limit: number,
 ): Promise<Map<string, any>> =>
     new Promise((resolve, reject) => {
         const data = new Map<string, any>()
         ;(<any>index)
-            .createReadStream({ gte: from, limit, keyAsBuffer: false })
+            .createReadStream({ gte: from, lte: to, limit, keyAsBuffer: false })
             .on('data', ({ key, value }) => data.set(key, value))
             .on('error', err => reject(err))
             .on('end', () => resolve(data))
@@ -78,38 +82,50 @@ function extractAttachments(pouchDoc): Partial<PageAttachments> {
     return { screenshot, favIcon }
 }
 
-async function processKey([pageKey, indexDoc]: [string, any]) {
+async function fetchPouchData(pageKey: string): Promise<Partial<ExportedPage>> {
     const pouchDoc = await db.get(pageKey, { attachments: true })
     const attachments = extractAttachments(pouchDoc)
 
-    // Grab all visit meta data
-    let visits: ExportedPageVisit[]
-    if (indexDoc.visits.size) {
-        const lookupMap: Map<string, VisitMeta> = await initLookupByKeys()([
-            ...indexDoc.visits,
-        ])
-
-        // Reshape found visit index docs to ExportedPageVisit shape
-        visits = [...lookupMap].map(([visitKey, { pageId, ...visit }]) => ({
-            timestamp: formatMetaKey(visitKey),
-            ...visit,
-        }))
-    } else {
-        visits = []
-    }
-
-    const page: ExportedPage = {
+    return {
         url: pouchDoc.url,
-        content: { ...pouchDoc.content },
-        visits: visits,
+        content: { ...(pouchDoc.content || {}) },
+        ...attachments,
+    }
+}
+
+async function fetchVisitsData(visits: string[]): Promise<ExportedPageVisit[]> {
+    // Grab all visit meta data
+    if (!visits.length) {
+        return []
+    }
+    const lookupMap: Map<string, VisitMeta> = await initLookupByKeys()(visits)
+
+    // Reshape found visit index docs to ExportedPageVisit shape
+    return [...lookupMap].map(([visitKey, { pageId, ...visit }]) => ({
+        timestamp: formatMetaKey(visitKey),
+        ...visit,
+    }))
+}
+
+async function processKey(
+    [pageKey, indexDoc]: [string, any],
+): Promise<Partial<ExportedPage>> {
+    const displayData = await fetchPouchData(pageKey).catch(
+        err => console.error(err) || {},
+    )
+
+    const visits = await fetchVisitsData([...indexDoc.visits]).catch(
+        err => console.error(err) || [],
+    )
+
+    return {
+        ...displayData,
+        visits,
         bookmark: indexDoc.bookmarks.size
             ? formatMetaKey([...indexDoc.bookmarks][0])
             : undefined,
         tags: [...indexDoc.tags].map(removeKeyType),
-        ...attachments,
     }
-
-    return page
 }
 
 const formatMetaKey = (key: string) => Number.parseInt(removeKeyType(key), 10)
