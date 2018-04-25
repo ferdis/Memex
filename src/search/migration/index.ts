0c7cb9d8006e072066c8f34ab0d9da9773bdaae1
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-04-25 12:40:19 +0700

    Set up auto-migration when browser is idle
    - seems to work OK!
    - whenever browser goes idle, migration will start/resume
    - if user comes back, it will save progress and stop
    - once it's finished a notification will show + it will auto switch user over to new index as search backend

--- src/search/migration/index.ts
+++ src/search/migration/index.ts
@@ -1,5 +1,29 @@
+import createNotif from '../../util/notifications'
+import { idleManager } from '../../util/idle'
 import { MigrationManager } from './migration-manager'
+import { getBackend } from '../'
+
 window['Migrate'] = MigrationManager
+
+const migrator = new MigrationManager({
+    onComplete() {
+        createNotif({
+            requireInteraction: true,
+            title: 'Memex Extension',
+            message:
+                'Your data has been migrated to our new and improved index!',
+        })
+
+        // Update global setting to force switch to using new index for all interface methods
+        getBackend._reset({ useOld: false })
+    },
+})
+
+// Schedule migrator to run when browser is idle
+idleManager.scheduleIdleCbs({
+    onIdle: () => migrator.start(),
+    onActive: () => migrator.stop(),
+})
 
 export * from './types'
 export { MigrationManager }
